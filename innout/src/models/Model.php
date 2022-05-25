57e954522d0d60b4893678a5443cbf0f6b960521
Arildo de Azevedo Júnior <66390130+ArildoJunior@users.noreply.github.com>
2022-05-25 15:36:50 -0300

    Inicio Login

--- innout/src/models/Model.php
+++ innout/src/models/Model.php
@@ -26,6 +26,12 @@ class Model {
     }
 
     
+    public static function getOne($filters = [], $columns = '*') {
+        $class = get_called_class();
+        $result = static::getResultSetFromSelect($filters, $columns);
+        return $result ? new $class($result -> fetch_assoc()) : null;
+    }
+
     public static function get($filters = [], $columns = '*') {
         $objects = [];
         $result = static::getResultSetFromSelect($filters, $columns);
@@ -37,7 +43,7 @@ class Model {
         }
         return $objects;
     }
-    
+
     public static function getResultSetFromSelect($filters = [], $columns = '*') {
         $sql = "SELECT ${columns} FROM " 
             . static::$tableName
