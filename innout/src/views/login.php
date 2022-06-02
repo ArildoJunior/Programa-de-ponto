8b9cabb7d17456645da9c6a132f0387ada93e248
Arildo de Azevedo Júnior <66390130+ArildoJunior@users.noreply.github.com>
2022-06-02 11:25:38 -0300

    Correção de erro no Validação de login #02

--- innout/src/views/login.php
+++ innout/src/views/login.php
@@ -25,22 +25,20 @@
                 <div class="form-group">
                     <label for="email">E-mail</label>
                     <input type="email" id="email" name="email"
-                    class="form-control 
-                    <?= isset($errors) ? ($errors->get('email') ? 'is-invalid' : '') : '' ?>"
-                    value="<?= $email ?>" placeholder="Informe o e-mail" autofocus>                    
+                        class="form-control <?= $errors['email'] ? 'is-invalid' : '' ?>"
+                        value="<?= $email ?>"
+                        placeholder="Informe o e-mail" autofocus>
                     <div class="invalid-feedback">
-                        <?= isset($errors) ? $errors->get('email') : '' ?>
+                        <?= $errors['email'] ?>
                     </div>
                 </div>
-            </div>
-            <div class="card-body">
                 <div class="form-group">
                     <label for="password">Senha</label>
                     <input type="password" id="password" name="password"
-                    class="form-control  <?= isset($errors) ? ($errors->get('password') ? 'is-invalid' : '') : '' ?>" 
-                    placeholder="Informe sua senha">                    
+                        class="form-control <?= $errors['password'] ? 'is-invalid' : '' ?>"
+                        placeholder="Informe a senha">
                     <div class="invalid-feedback">
-                        <?= isset($errors) ? $errors->get('password') : '' ?>
+                        <?= $errors['password'] ?>
                     </div>
                 </div>
             </div>
