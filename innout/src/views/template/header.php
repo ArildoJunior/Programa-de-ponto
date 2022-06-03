58607aefe5a15c571ccac17620240ca45964d22c
Arildo de Azevedo Júnior <66390130+ArildoJunior@users.noreply.github.com>
2022-06-03 11:03:22 -0300

    Templates

--- innout/src/views/template/header.php
+++ innout/src/views/template/header.php
@@ -26,19 +26,18 @@
 
         <div class="spacer"></div>
         <div class="dropdown">
-            <div class="dropdown-button">
-                <span>Usuário Mock</span>
-                <i class="icofont-simple-down ml-2"></i>
-            </div>
+            <div class="dropdown-button"></div>
+            <span class="ml-3">Usuário Mock</span>
+            <i class="icofont-simple-down mx-2"></i>
             <div class="dropdown-content">
                 <ul class="nav-list">
                     <li class="nav-item">
                         <a href="logout.php">
-                            <i class="iconfont-logout mr-2"></i>
+                            <i class="icofont-logout mr-2"></i>
                             Sair
                         </a>
                     </li>
-                </ul>
+                </ul>               
             </div>
         </div>
     </header>
