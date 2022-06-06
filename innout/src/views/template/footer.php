5997d8db5fe2c19a5aaccad66d276c2a0ee3c533
Arildo de Azevedo Júnior <66390130+ArildoJunior@users.noreply.github.com>
2022-06-06 11:22:55 -0300

    Finalização

--- innout/src/views/template/footer.php
+++ innout/src/views/template/footer.php
@@ -3,5 +3,6 @@
     <span><i class="icofont-heart text-danger mx-1"></i></span>
     <span>por JR</span>
 </footer>
+<script src="assets/js/app.js"></script>
 </body>
 </html>
--- innout/src/views/template/header.php
+++ innout/src/views/template/header.php
@@ -2,33 +2,37 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/css/comum.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/icofont.min.css">
     <link rel="stylesheet" href="assets/css/template.css">
     <title>In N' Out</title>
 </head>
-<body class="hide-sidebar">
+<body>
     <header class="header">
         <div class="logo">
             <i class="icofont-travelling mr-2"></i>
-                <span class="font-weight-light">In</span>
-                <span class="font-weight-bold mx-2">N'</span>
-                <span class="font-weight-light">Out</span>
+            <span class="font-weight-light">In </span>
+            <span class="font-weight-bold mx-2">N'</span>
+            <span class="font-weight-light">Out</span>
             <i class="icofont-runner-alt-1 ml-2"></i>
         </div>
-
         <div class="menu-toggle mx-3">
             <i class="icofont-navigation-menu"></i>
         </div>
-
         <div class="spacer"></div>
         <div class="dropdown">
-            <div class="dropdown-button"></div>
-            <span class="ml-3">Usuário Mock</span>
-            <i class="icofont-simple-down mx-2"></i>
+            <div class="dropdown-button">
+                <img class="avatar" 
+                    src="<?= "http://www.gravatar.com/avatar.php?gravatar_id="
+                    . md5(strtolower(trim($_SESSION['user']->email))) ?>" alt="user">
+                <span class="ml-3">
+                    <?= $_SESSION['user']->name ?>
+                </span>
+                <i class="icofont-simple-down mx-2"></i>
+            </div>
             <div class="dropdown-content">
                 <ul class="nav-list">
                     <li class="nav-item">
@@ -37,7 +41,7 @@
                             Sair
                         </a>
                     </li>
-                </ul>               
+                </ul>
             </div>
         </div>
     </header>
